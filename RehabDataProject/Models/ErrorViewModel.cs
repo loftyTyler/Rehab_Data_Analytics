a8850e453382f9a49d583113f84af231ad13d9a9
Tyler Lofthouse <telofthouse@gmail.com>
2023-05-30 12:05:04 -0500

    Add project files.

---
+++ RehabDataProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RehabDataProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
