f121b9268e13ad23b8311d118651b33c8bf1efe1
Tyler Lofthouse <telofthouse@gmail.com>
2023-05-30 16:43:44 -0500

    Hard coded database table to convert migration into sql table

---
+++ RehabDataProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RehabDataProject.Models;
+
+namespace RehabDataProject.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Patient_Info> Patients_Info { get; set; }
+    }
+}
---
+++ RehabDataProject/Models/Patient_Info.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RehabDataProject.Models
+{
+    public class Patient_Info
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public double KneeROM { get; set; }
+        public int KneeStrength { get; set; }
+        public double LowerbackExtROM { get; set; }
+        public double LowerBackFlexionROM { get; set; }
+        public int PainFreeWeightLifted { get; set; }
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
+    }
+}
