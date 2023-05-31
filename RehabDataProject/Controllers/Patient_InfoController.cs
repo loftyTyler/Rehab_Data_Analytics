07c1e10ec4353dc2610e228d4f18754247dc9181
Tyler Lofthouse <telofthouse@gmail.com>
2023-05-30 19:02:30 -0500

    Added controller

---
+++ RehabDataProject/Controllers/Patient_InfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RehabDataProject.Controllers
+{
+    public class Patient_InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
