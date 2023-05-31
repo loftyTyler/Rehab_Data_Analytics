0a5b16698b0f99cbf2fbf14d70b7b57d1ee342da
Tyler Lofthouse <telofthouse@gmail.com>
2023-05-30 19:40:32 -0500

    Displayed Table Data on View

--- RehabDataProject/Controllers/Patient_InfoController.cs
+++ RehabDataProject/Controllers/Patient_InfoController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RehabDataProject.Data;
+using RehabDataProject.Models;
 
 namespace RehabDataProject.Controllers
 {
     public class Patient_InfoController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public Patient_InfoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Patient_Info> objPatientsList = _db.Patients_Info;
+            return View(objPatientsList);
         }
     }
 }
